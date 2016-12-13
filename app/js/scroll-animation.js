71780e1468b23861deb565f501706f328133bb70
rinat_y <qwwert@mail.rui>
2016-12-13 10:26:10 +0300

    09.12.16 18:27 Нормальный комит

---
+++ app/js/scroll-animation.js
@@ -0,0 +1,62 @@
+/**
+ * Created by rinat_y on 12/13/16.
+ */
+$(document).ready(function(){
+
+    $('#element-to-animate').css('opacity', 0);
+
+    $('#element-to-animate').waypoint(function() {
+        $('#element-to-animate').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+    $('#element-to-animate2').css('opacity', 0);
+
+    $('#element-to-animate2').waypoint(function() {
+        $('#element-to-animate2').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+    $('#element-to-animate3').css('opacity', 0);
+
+    $('#element-to-animate3').waypoint(function() {
+        $('#element-to-animate3').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+    $('#element-to-animate4').css('opacity', 0);
+
+    $('#element-to-animate4').waypoint(function() {
+        $('#element-to-animate4').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+    $('#element-to-animate5').css('opacity', 0);
+
+    $('#element-to-animate5').waypoint(function() {
+        $('#element-to-animate5').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+    $('#element-to-animate6').css('opacity', 0);
+
+    $('#element-to-animate6').waypoint(function() {
+        $('#element-to-animate6').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+
+    $('#element-to-animate7').css('opacity', 0);
+
+    $('#element-to-animate7').waypoint(function() {
+        $('#element-to-animate7').addClass('fadeIn');
+    }, { offset: '50%' });
+
+
+
+    $('#element-to-animate8').css('opacity', 0);
+
+    $('#element-to-animate8').waypoint(function() {
+        $('#element-to-animate8').addClass('fadeIn');
+    }, { offset: '50%' });
+});
